c5ebde40e4478edc93cf40270a57854299c22e25
LLambert <lambertlb@aol.com>
2023-11-17 08:16:07 -0600

    adjusted settings

--- CharacterTemplates/scripts/PersonalInfo.py
+++ CharacterTemplates/scripts/PersonalInfo.py
@@ -5,56 +5,56 @@ from configurator.Services import Services
 
 
 class PersonalInfo(CharacterEntity):
-	def __init__(self):
-		super().__init__()
-		self.Deity = None
-		self.Race = None
-		self._deityScript = None
-		self._raceScript = None
+    def __init__(self):
+        super().__init__()
+        self.Deity = None
+        self.Race = None
+        self._deityScript = None
+        self._raceScript = None
 
-	def register(self):
-		super().register()
-		if not self._deityScript and self.Deity != '':
-			self._deityScript = Entity.instanceFromScript('CharacterTemplates.scripts.Deities.' + self.Deity)
-		if self.Deity != '':
-			self._deityScript.register()
-		if not self._raceScript and self.Race != '':
-			self._raceScript = Entity.instanceFromScript('CharacterTemplates.scripts.Races.' + self.Race)
-		if self.Race != '':
-			self._raceScript.register()
+    def register(self):
+        super().register()
+        if not self._deityScript and self.Deity != '':
+            self._deityScript = Entity.instanceFromScript(
+                'CharacterTemplates.scripts.Deities.' + self.Deity)
+        if self.Deity != '':
+            self._deityScript.register()
+        if not self._raceScript and self.Race != '':
+            self._raceScript = Entity.instanceFromScript(
+                'CharacterTemplates.scripts.Races.' + self.Race)
+        if self.Race != '':
+            self._raceScript.register()
 
-	def update(self):
-		super().register()
-		if self._raceScript:
-			self._raceScript.update()
-		if self._deityScript:
-			self._deityScript.update()
+    def update(self):
+        super().register()
+        if self._raceScript:
+            self._raceScript.update()
+        if self._deityScript:
+            self._deityScript.update()
 
-	def getDataForProperty(self, propertyName, propertyType):
-		if propertyName == 'Deity':
-			return self.getPropertyDataForDeity(propertyName, propertyType)
-		if propertyName == 'Race':
-			return self.getPropertyDataForRace(propertyName, propertyType)
-		if propertyName == 'Gender':
-			return self.getPropertyDataForGender(propertyName, propertyType)
-		return super().getDataForProperty(propertyName, propertyType)
+    def getDataForProperty(self, propertyName, propertyType):
+        if propertyName == 'Deity':
+            return self.getPropertyDataForDeity(propertyName, propertyType)
+        if propertyName == 'Race':
+            return self.getPropertyDataForRace(propertyName, propertyType)
+        if propertyName == 'Gender':
+            return self.getPropertyDataForGender(propertyName, propertyType)
+        return super().getDataForProperty(propertyName, propertyType)
 
-	def getPropertyDataForDeity(self, propertyName, propertyType):
-		templatePath = Services.getConfigurationManager().getValue(CharacterManagerConfig.sourcesKey,
-																CharacterManagerConfig.characterTemplateDirectoryKey)
-		path = templatePath + '/scripts/Deities'
-		deities = Entity.getListOfClassesFromDirectory(path, Entity)
-		return (propertyName, 'composite', self.Deity, deities)
+    def getPropertyDataForDeity(self, propertyName, propertyType):
+        templatePath = Services.getConfigurationManager().getValue(CharacterManagerConfig.sourcesKey, CharacterManagerConfig.characterTemplateDirectoryKey)
+        path = templatePath + '/scripts/Deities'
+        deities = Entity.getListOfClassesFromDirectory(path, Entity)
+        return (propertyName, 'composite', self.Deity, deities)
 
-	def getPropertyDataForRace(self, propertyName, propertyType):
-		templatePath = Services.getConfigurationManager().getValue(CharacterManagerConfig.sourcesKey,
-																CharacterManagerConfig.characterTemplateDirectoryKey)
-		path = templatePath + '/scripts/Races'
-		races = Entity.getListOfClassesFromDirectory(path, Entity)
-		return (propertyName, 'composite', self.Race, races)
+    def getPropertyDataForRace(self, propertyName, propertyType):
+        templatePath = Services.getConfigurationManager().getValue(CharacterManagerConfig.sourcesKey, CharacterManagerConfig.characterTemplateDirectoryKey)
+        path = templatePath + '/scripts/Races'
+        races = Entity.getListOfClassesFromDirectory(path, Entity)
+        return (propertyName, 'composite', self.Race, races)
 
-	def getPropertyDataForGender(self, propertyName, propertyType):
-		return (propertyName, 'composite', self.Gender, ['Male', 'Female', 'Neuter'])
+    def getPropertyDataForGender(self, propertyName, propertyType):
+        return (propertyName, 'composite', self.Gender, ['Male', 'Female', 'Neuter'])
 
-	def isValidPropertyChange(self, propertyName, propertyData):
-		return True
+    def isValidPropertyChange(self, propertyName, propertyData):
+        return True
